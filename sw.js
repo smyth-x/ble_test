8ef9d3fea079b2e8994023b820d1f829bb572d3d
smyth-x <161477651+smyth-x@users.noreply.github.com>
2024-05-09 10:49:46 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,13 @@
-const CACHE_NAME = `JMX24-v2`;
+const CACHE_NAME = `JMX24-v3`;
 
 // Use the install event to pre-cache all initial resources.
 self.addEventListener('install', event => {
   event.waitUntil((async () => {
     const cache = await caches.open(CACHE_NAME);
     cache.addAll([
-      './',
-     
-      './style.css'
+      '',
+     'index.html',
+      'style.css'
     ]);
   })());
 });
