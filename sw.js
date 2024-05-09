f1b888841938e79e0622b8c7f7199ef3ef267f2e
smyth-x <161477651+smyth-x@users.noreply.github.com>
2024-05-09 10:07:36 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -5,9 +5,9 @@ self.addEventListener('install', event => {
   event.waitUntil((async () => {
     const cache = await caches.open(CACHE_NAME);
     cache.addAll([
-      '/',
+      './',
      
-      '/style.css'
+      './style.css'
     ]);
   })());
 });
