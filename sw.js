d9051b745787caa242663e8d5d97ac90cce6523a
smyth-x <161477651+smyth-x@users.noreply.github.com>
2024-05-14 17:53:11 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = `JMX24-v07`;
+const CACHE_NAME = `JMX24-v08`;
 
 // Use the install event to pre-cache all initial resources.
 self.addEventListener('install', event => {
@@ -12,8 +12,8 @@ self.addEventListener('install', event => {
      'index.html',
       'style.css',
       'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
-      'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js',
-      'https://tile.openstreetmap.org/{z}/{x}/{y}.png'
+      'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js'
+     
     ]);
   })());
 });
